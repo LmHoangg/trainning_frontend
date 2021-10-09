31bfb61ff2f5a082bee9905201d86f9584a5bb52
Hoang Lam <rutatut2000@gmail.com>
2021-10-09 22:06:17 +0700

    update review/v1

--- CRUD_USER/index.js
+++ CRUD_USER/index.js
@@ -8,9 +8,9 @@ const container = document.querySelector(".container");
 const showPost = document.querySelector(".show-detail");
 let itemPost = "";
 let postDetail = "";
-const renderPost = (posts) => {
-    posts.forEach((post) => {
-        itemPost += `<div class="card col-md-12 col-lg-12 row mb-3 mt-3">
+const renderPosts = (posts) => {
+  posts.forEach((post) => {
+    itemPost += `<div class="card col-md-12 col-lg-12 row mb-3 mt-3">
                 <div class="card-body" data-id=${post.id}>
                     <h5 class="card-title">${post.title}</h5>
                     <p class="card-text">${post.body}</p>
@@ -19,11 +19,22 @@ const renderPost = (posts) => {
                     <a href="#" class="btn btn-success" id="show-post">Show</a>
                 </div>
             </div>`;
-    });
-    postsList.innerHTML = itemPost;
+  });
+  postsList.innerHTML = itemPost;
+};
+const renderPost = (post) => {
+  postsList.innerHTML += `<div class="card col-md-12 col-lg-12 row mb-3 mt-3">
+                <div class="card-body" data-id=${post.id}>
+                    <h5 class="card-title">${post.title}</h5>
+                    <p class="card-text">${post.body}</p>
+                    <a href="#" class="btn btn-primary" id="edit-post">Edit</a>
+                    <a href="#" class="btn btn-danger" id="delete-post">Delete</a>
+                    <a href="#" class="btn btn-success" id="show-post">Show</a>
+                </div>
+            </div>`;
 };
 const renderDetailPost = (post) => {
-    postDetail += ` <h5>Detail post
+  postDetail += `<h5>Detail post
             <i class="fas fa-times-circle close-post"></i>
         </h5><div class="card">
             <div class="card-body">
@@ -31,75 +42,89 @@ const renderDetailPost = (post) => {
                 <p class="card-text ">${post.body}</p>
             </div>
         </div>`;
-    showPost.innerHTML = postDetail;
-}
+  showPost.innerHTML = postDetail;
+};
 fetch(url)
-    .then((response) => response.json())
-    .then((data) => renderPost(data))
-postsList.addEventListener("click", function(e) {
-    e.preventDefault();
-    let delIsPressed = e.target.id === "delete-post";
-    let editIsPressed = e.target.id === "edit-post";
-    let showIsPressed = e.target.id === "show-post";
-    let id = e.target.parentElement.dataset.id;
-    console.log(id);
-    if (delIsPressed) {
-        fetch(`${url}/${id}`, {
-                method: 'DELETE',
-            }).then(response => response.json())
-            .then(() => location.reload());
-    }
-    if (editIsPressed) {
-        const titleContent = e.target.previousElementSibling;
-        const bodyContent = e.target.previousElementSibling.previousElementSibling;
-        titleValue.value = titleContent.textContent;
-        bodyValue.value = bodyContent.textContent;
-    }
-    if (showIsPressed) {
-        container.classList.add("isHide");
-        showPost.classList.add("isShow");
-        fetch(`${url}/${id}`)
-            .then((response) => response.json())
-            .then((data) => renderDetailPost(data));
-    }
-    btnSubmit.addEventListener("click", function(e1) {
+  .then((response) => response.json())
+  .then((data) => renderPosts(data));
+postsList.addEventListener("click", function (e) {
+  e.preventDefault();
+  let delIsPressed = e.target.id === "delete-post";
+  let editIsPressed = e.target.id === "edit-post";
+  let showIsPressed = e.target.id === "show-post";
+  let id = e.target.parentElement.dataset.id;
+
+  switch (true) {
+    case delIsPressed:
+      fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          document.querySelector(`[data-id="${id}"]`).parentElement.remove();
+        })
+        .catch((error) => console.log(error.message));
+      break;
+    case editIsPressed:
+      const bodyContent = e.target.previousElementSibling;
+      const titleContent =
+        e.target.previousElementSibling.previousElementSibling;
+      titleValue.value = titleContent.textContent;
+      bodyValue.value = bodyContent.textContent;
+      btnSubmit.addEventListener("click", function (e1) {
         e1.preventDefault();
         fetch(`${url}/${id}`, {
-                method: 'PATCH',
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    title: titleValue.value,
-                    body: bodyValue.value
-                })
-            }).then(response => response.json())
-            .then(() => location.reload());
-    })
-})
-formPost.addEventListener("submit", function(e) {
-    e.preventDefault();
-    fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: titleValue.value,
-                body: bodyValue.value
-            })
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: titleValue.value,
+            body: bodyValue.value,
+          }),
         })
-        .then(response => response.json())
-        .then(data => {
-            const dataArr = [];
-            dataArr.push(data)
-            renderPost(dataArr);
-        })
-    titleValue.value = '';
-    bodyValue.value = '';
-})
+          .then((response) => response.json())
+          .catch((error) => console.log(error.message));
+        let post = document.querySelector(`[data-id="${id}"]`);
+        post.firstElementChild.textContent = titleValue.value;
+        post.firstElementChild.nextElementSibling.textContent = bodyValue.value;
+        titleValue.value = "";
+        bodyValue.value = "";
+      });
+      break;
+    case showIsPressed:
+      container.classList.add("isHide");
+      showPost.classList.add("isShow");
+      fetch(`${url}/${id}`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => renderDetailPost(data))
+        .catch((error) => console.log(error.message));
+  }
+});
+formPost.addEventListener("submit", function (e) {
+  e.preventDefault();
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: titleValue.value,
+      body: bodyValue.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => renderPost(data));
+  titleValue.value = "";
+  bodyValue.value = "";
+});
 const close = document.querySelector(".close-post");
-close.addEventListener("click", function(e) {
-    container.classList.remove("isHide");
-    showPost.classList.remove("isShow");
+close.addEventListener("click", function (e) {
+  container.classList.remove("isHide");
+  showPost.classList.remove("isShow");
 });
